aa57a7b274e761eec6ed4e7c305c8b607e6207d2
silencer <nakonecnijdmitro88@gmail.com>
2024-12-17 16:27:08 +0200

    Implement "Complete" button and "Exit"

--- gui.py
+++ gui.py
@@ -15,11 +15,15 @@ in_box = sg.InputText(tooltip="Enter text...", key='to_do')
 # Buttons
 but_add = sg.Button('Add')
 but_edit = sg.Button('Edit')
+but_complete = sg.Button("Complete")
+but_exit = sg.Button("Exit")
+
+layout = [[lebel], [in_box, but_add],
+          [[but_edit, but_complete], list_box],
+          [but_exit]]
 
 window = sg.Window("My App to-do",
-                   layout=[[lebel],
-                           [in_box, but_add],
-                           [list_box, but_edit]],
+                   layout=layout,
                    font=('Helvetica', 20))
 
 
@@ -44,8 +48,18 @@ while True:
             todo[index] =  new_todo
             todo_m.do_todo(todo)
             window['todos'].update(values=todo)
+        case 'Complete':
+            ch_val = values['todos'][0]
+
+            todo = todo_m.get_todo()
+            todo.remove(ch_val)
+            todo_m.do_todo(todo)
+            window['todos'].update(values=todo)
+            window['to_do'].update(value='')
         case 'todos':
             window['to_do'].update(value=values['todos'][0])
-        case sg.WIN_CLOSED:
+        case 'Exit':
             break
+        case sg.WIN_CLOSED:
+            exit()
 
